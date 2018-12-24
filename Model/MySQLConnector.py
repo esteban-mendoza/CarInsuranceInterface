a58fe5720329fa146550e6b2a5191aa31525259b
estebanmend <esteban.mendoza@outlook.com>
2018-12-24 16:15:13 -0600

    Add MySQLConnector class

--- Model/MySQLConnector.py
+++ Model/MySQLConnector.py
@@ -1,5 +1,6 @@
-import mysql.connector
+from mysql.connector.connection import MySQLConnection
 from mysql.connector import errorcode
+import mysql.connector
 
 
 class MySQLConnector:
@@ -8,20 +9,45 @@ class MySQLConnector:
         'user': 'root',
         'password': 'password',
         'host': 'localhost',
-        'database': 'proyecto',
-        'raise_on_warnings': True
+        'database': 'fciencias'
     }
 
-    def __init__(cls, *args, **kargs):
-        if cls.connection is None:
-            try:
-                cls.cnx = mysql.connector.connect(**self.config)
+    def __init__(self):
+        try:
+            self.connection = MySQLConnection(**self.config)
+            print("Connection created")
 
-            except mysql.connector.Error as err:
-                if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                    print("Something is wrong with your user name or password")
-                elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                    print("Database does not exist")
-                else:
-                    print(err)
-        return cls.instance
+            self.cur = self.connection.cursor()
+
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+
+    def query(self, query):
+        self.cur.execute(query)
+
+    def insert(self, query):
+        self.cur.execute(query)
+
+    def commit(self):
+        self.cur.commit()
+
+    def __del__(self):
+        self.cur.close()
+        self.connection.close()
+        print("Connection closed")
+
+
+if __name__ == '__main__':
+    cnx = MySQLConnector()
+
+    cnx.query("SELECT * FROM alumnos")
+
+    for row in cnx.cur:
+        print(row)
+
+    del cnx
